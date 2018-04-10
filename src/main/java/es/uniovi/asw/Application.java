6df0ae84d088075ac6277773a3764e7df9672332
Jose Labra <jelabra@gmail.com>
2018-04-10 07:10:22 +0200

    It exports metrics at actuator/prometheus

--- src/main/java/es/uniovi/asw/Application.java
+++ src/main/java/es/uniovi/asw/Application.java
@@ -4,13 +4,9 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
-import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
 
 @EnableAutoConfiguration
 @SpringBootApplication
-@EnablePrometheusEndpoint
-@EnableSpringBootMetricsCollector
 public class Application {
 
   public static void main(String[] args) {
